65aedf97f32696e926524e517fe9349f03fef395
akhtarraja513 <akhtarraja513@gmail.com>
2022-08-09 15:59:29 +0530

    Final

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 const App = () => {
     return (
-        <div id="root">
+        <>
             <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
-        </div>
+        </>
     )
 }
 
