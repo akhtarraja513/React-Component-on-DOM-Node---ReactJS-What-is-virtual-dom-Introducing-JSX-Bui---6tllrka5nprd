a3f8d0ce5589eced80c58de276b478d849127252
akhtarraja513 <akhtarraja513@gmail.com>
2022-08-09 16:30:49 +0530

    Chal ja

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 function App() {
     return (
-        <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+        <p className="App">Now I can render any React component on any DOM node I want using ReactDOM.render</p>
     )
 }
 
